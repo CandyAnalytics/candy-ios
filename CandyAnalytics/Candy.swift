11ddf5308b33fcbea46b4c06294f3c3401828821
Oscar Swanros <oscar@swanros.com>
2017-02-21 18:48:38 -0600

    Create Candy file and Configuration structs

---
+++ CandyAnalytics/Candy.swift
@@ -0,0 +1,27 @@
+//
+//  Candy.swift
+//  candy-ios
+//
+//  Created by Oscar Swanros on 2/21/17.
+//  Copyright © 2017 Oscar Swanros. All rights reserved.
+//
+
+public struct Candy {
+    public struct Configuration {
+        public let url: URL
+        public let key: String
+        public let application: UIApplication?
+        
+        public init(url: URL, key: String, application: UIApplication? = nil) {
+            self.url = url
+            self.key = key
+            self.application = application
+        }
+    }
+    
+    fileprivate static var configuration: Candy.Configuration?
+    
+    public static func setup(configuration: Candy.Configuration) {
+        Candy.configuration = configuration
+    }
+}
