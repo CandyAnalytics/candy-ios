6edc86dcdd70d6d02720dada4db597f243b69485
Oscar Swanros <oscar@swanros.com>
2017-02-21 18:12:37 -0600

    Create Xcode project

---
+++ candy-ios/CandyAnalytics/CandyAnalytics.h
@@ -0,0 +1,19 @@
+//
+//  CandyAnalytics.h
+//  CandyAnalytics
+//
+//  Created by Oscar Swanros on 2/21/17.
+//  Copyright © 2017 Oscar Swanros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CandyAnalytics.
+FOUNDATION_EXPORT double CandyAnalyticsVersionNumber;
+
+//! Project version string for CandyAnalytics.
+FOUNDATION_EXPORT const unsigned char CandyAnalyticsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CandyAnalytics/PublicHeader.h>
+
+
